b735679153e44cc245afa8d28285d5eeed8368e1
physikerwelt (Moritz Schubotz) <wiki@physikerwelt.de>
2013-11-06 16:46:13 +0100

    fixup! New hadoop puppet script
    Change-Id: I9aa7466e64ff6a4cf5736a74922e9f0c69534705

--- puppet/manifests/hadoop.pp
+++ puppet/manifests/hadoop.pp
@@ -2,10 +2,10 @@ class my::hadoop {
     class { 'cdh4::hadoop':
         # Must pass an array of hosts here, even if you are
         # not using HA and only have a single NameNode.
-        namenode_hosts     => ['namenode1.domain.org'],
+        namenode_hosts     => ['localhost'],
         datanode_mounts    => ['/var/lib/hadoop/data/a'],
         # You can also provide an array of dfs_name_dirs.
-        dfs_name_dir       => '/var/lib/hadoop/name',
+        dfs_name_dir       => '/var/lib/hadoop',
     }
 }
 
