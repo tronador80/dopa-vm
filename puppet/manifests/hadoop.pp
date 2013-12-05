5e1bbdf32df83ea67451b4848028aaf7afcbbaab
Moritz Schubotz <github@ckurs.de>
2013-12-05 22:53:26 +0100

    fixup: Typo in hadoop.pp

--- puppet/manifests/hadoop.pp
+++ puppet/manifests/hadoop.pp
@@ -3,7 +3,7 @@ class my::hadoop {
         # Must pass an array of hosts here, even if you are
         # not using HA and only have a single NameNode.
         namenode_hosts     => ['localhost'],
-        datanode_mounts    => ['/var/lib/hadoop/data/a'],
+        datanode_mounts    => ['/var/lib/hadoop/data'],
         # You can also provide an array of dfs_name_dirs.
         dfs_name_dir       => '/var/lib/hadoop',
     }
